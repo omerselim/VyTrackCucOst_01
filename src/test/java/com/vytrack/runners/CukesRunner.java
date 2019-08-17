0cae2d0b5a2931cbf820a42d9c9589d956e39fe5
omerselim <49600938+omerselim@users.noreply.github.com>
2019-08-17 13:03:52 -0400

    commit_copyPast_190817

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -6,25 +6,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-cucumber-reports" },
 
-        plugin = {
-                "json:target/cucumber.json",
-                "html:target/default-cucumber-reports"
-        },
-//        tags = {"@navigation"},
-//        tags = {"@VYT-4121"},
-        tags = {"@vehicles"},
-        features = {"src/test/resources/features" //to specify where are the features
-        },
-        //feature contains scenarios
-        //every scenario is like a test
-        //where is the implementation for features
+//        tags = {"@dashboard-navigation"},
+//        tags = {"@VYT-4121_login"},
+//        tags = {"@vehicles"},
+
+        features = {"src/test/resources/features"},      // to specify where are the features. feature contains scenarios, every scenario is like a test
         glue = {"com/vytrack/step_definitions"},
-        //dry run - to generate step definitions automatically
-        //you will see them in the console output
-//        dryRun = true
-        dryRun = false
 
+//       dryRun = true      //dryRun = true  --> generates step definitions automatically
+        dryRun = false
 )
 public class CukesRunner {
 
